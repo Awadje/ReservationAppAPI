321b5f743b854af29fb03cd5f340d9cafb56bc05
Michael Awad <awad.michael@216.nl>
2018-05-07 17:02:45 +0200

    Tables now realtime

--- TableService/service.js
+++ TableService/service.js
@@ -2,15 +2,10 @@ const feathers = require('@feathersjs/feathers');
 const express = require('@feathersjs/express');
 const socketio = require('@feathersjs/socketio');
 
-const mongoose = require('mongoose');
-const service = require('feathers-mongoose');
+const MongoClient = require('mongodb').MongoClient;
+const service = require('feathers-mongodb');
 
 const Model = require('./TableSchema');
-
-mongoose.Promise = global.Promise;
-
-// Connect to your MongoDB instance(s)
-mongoose.connect('mongodb://localhost:27017/feathers');
 
 // Create an Express compatible Feathers application instance.
 const app = express(feathers());
@@ -18,31 +13,44 @@ const app = express(feathers());
 // Turn on JSON parser for REST services
 app.use(express.json());
 // Turn on URL-encoded parser for REST services
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 // Enable REST services
 app.configure(express.rest());
-// Enable Socket.io services
-app.configure(socketio());
-// Connect to the db, create and register a Feathers service.
-app.use('/tables', service({
-  Model,
-  lean: true, // set to false if you want Mongoose documents returned
-  paginate: {
-    default: 2,
-    max: 4
-  }
-}));
+
+// Set up an error handler that gives us nicer errors
 app.use(express.errorHandler());
 
-// Create a dummy Message
-app.service('tables').create({
-  text: 'Table created on server'
-}).then(function(table) {
-  console.log('Created message', table);
-});
+// Enable Socket.io
+app.configure(socketio());
+
+// On any real-time connection, add it to the `everybody` channel
+app.on('connection', connection => app.channel('everybody').join(connection));
+
+// Publish all events to the `everybody` channel
+app.publish(() => app.channel('everybody'));
+
+// Connect to the db, create and register a Feathers service.
+app.use('/tables', service({
+  paginate: {
+    default: 15,
+    max: 25
+  }
+}));
+
+// Connect to your MongoDB instance(s)
+MongoClient.connect('mongodb://localhost:27017/tables')
+  .then(function (client) {
+    // Set the model now that we are connected
+    app.service('tables').Model = client.db('tables').collection('tables');
+
+  }).catch(error => console.error(error));
+
+  // A basic error handler, just like Express
+  app.use(express.errorHandler());
 
 // Start the server.
 const port = 3030;
+
 app.listen(port, () => {
-    console.log(`Feathers server listening on port ${port}`);
+  console.log(`Feathers server listening on port ${port}`);
 });
